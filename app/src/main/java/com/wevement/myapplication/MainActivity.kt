77e1ffe84a835de7b97daf66aa958b473c3c978b
2inlee <kbs@kakao.com>
2023-10-02 19:15:06 +0900

    Practice compose mutableState

--- app/src/main/java/com/wevement/myapplication/MainActivity.kt
+++ app/src/main/java/com/wevement/myapplication/MainActivity.kt
@@ -1,81 +1,41 @@
 package com.wevement.myapplication
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.wevement.myapplication.ui.theme.MyApplicationTheme
+import java.io.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            var isFavorite by rememberSaveable {
-                mutableStateOf(false)
+        setContent{
+            val (text, setValue) = remember {
+                mutableStateOf("")
             }
-            ImageCard(modifier = Modifier
-                .fillMaxWidth(0.5f)
-                .padding(16.dp),
-                isFavorite= isFavorite,
-                ){favorite -> isFavorite = favorite}
 
-        }
-    }
-}
-
-@Composable
-fun ImageCard(
-    modifier: Modifier = Modifier,
-    isFavorite:Boolean,
-    onTabFavorite: (Boolean) -> Unit){
-    Card(
-        modifier = Modifier,
-        shape = RoundedCornerShape(8.dp),
-        elevation = 5.dp,
-    ) {
-        Box(
-            modifier = Modifier
-                .height(200.dp)
-                .width(200.dp)
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.inlee_photo2),
-                contentDescription = "poster]",
-                contentScale = ContentScale.Crop
-            )
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.TopEnd,
-            )
-            {
-                IconButton(onClick = {
-                    onTabFavorite(!isFavorite)
-                }) {
-                    Icon(imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder, contentDescription = "favorite",
-                        tint = Color.White)
+            Column(
+                modifier =  Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                TextField(
+                    value = text,
+                    onValueChange = setValue,
+                )
+                Button(onClick = {  }) {
+                    Text(text = "Click me")
                 }
             }
         }
